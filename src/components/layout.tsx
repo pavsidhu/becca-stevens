efd7f59ad8a958467531df4b8e83b377f1a614f5
Pav Sidhu <pav@pavsidhu.com>
2020-04-15 18:19:31 +0100

    Add global style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,35 @@
 import React from "react"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: #efefef;
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
 
 export default function Layout({ children }) {
-  return <main>{children}</main>
+  return (
+    <>
+      <GlobalStyle />
+      <main>{children}</main>
+    </>
+  )
 }
