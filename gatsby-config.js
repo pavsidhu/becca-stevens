060b3ba7a4a417bfa027b4d2ca1ba5323d596e0c
Pav Sidhu <pav@pavsidhu.com>
2020-04-15 17:38:58 +0100

    Remove boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,6 @@ module.exports = {
         background_color: `#CA8080`,
         theme_color: `#CA8080`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
