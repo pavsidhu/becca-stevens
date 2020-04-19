83d5a841b7218fa357b090d5b4113a7a4a26178f
Pav Sidhu <pav@pavsidhu.com>
2020-04-19 17:45:40 +0100

    Add basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,45 @@
 import React from "react"
 import styled from "styled-components"
 
+import InstagramIcon from "../images/icons/instagram.svg"
 import { colors, size } from "../styles"
 
-const Container = styled.footer``
+const Container = styled.footer`
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: ${colors.pink};
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+`
+
+const Copyright = styled.p`
+  font-size: 1.2rem;
+  letter-spacing: 0.2rem;
+  text-transform: uppercase;
+  color: ${colors.black};
+`
+
+const SocialIcon = styled.a`
+  width: 32px;
+  height: 32px;
+  padding: 8px;
+`
+
+const Instagram = styled(InstagramIcon)`
+  width: 100%;
+  height: 100%;
+  fill: ${colors.black};
+`
 
 export default function Footer() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Copyright>© {new Date().getFullYear()} Maintain Fitness</Copyright>
+
+      <SocialIcon href="https://instagram.com/traintomaintain">
+        <Instagram />
+      </SocialIcon>
+    </Container>
+  )
 }
