9d12e8b18b49f4c664717433e0d64ab123d35856
Pav Sidhu <pav@pavsidhu.com>
2020-04-20 17:02:27 +0100

    Move Title from blog post section to own component

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -1,19 +1,11 @@
 import React from "react"
 import styled from "styled-components"
 
+import { Title } from "."
 import { colors, size } from "../styles"
 
 const Container = styled.article`
   margin: 80px 0;
-`
-
-const Title = styled.h2`
-  text-align: center;
-  text-transform: uppercase;
-  letter-spacing: 0.4rem;
-  font-size: 3.5rem;
-  font-weight: 300;
-  color: ${colors.black};
 `
 
 export default function BlogPosts() {
