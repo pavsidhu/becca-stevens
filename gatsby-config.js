61b9c7312c4ff4c90a0221e402cbba44606b57cf
Pav Sidhu <pav@pavsidhu.com>
2020-04-20 17:02:58 +0100

    Organise Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -23,8 +22,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -36,6 +33,9 @@ module.exports = {
         display: `minimal-ui`,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
