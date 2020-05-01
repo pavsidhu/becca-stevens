6d3be3317757da1a8109b03b7e38207e8183eca4
Pav Sidhu <pav@pavsidhu.com>
2020-05-01 17:36:29 +0100

    Ignore seamless-scroll-polyfill on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,28 @@
 const { createFilePath } = require("gatsby-source-filesystem")
 
+// Add slug to blog post nodes
 exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === `MarkdownRemark`) {
     actions.createNodeField({
       node,
       name: `slug`,
       value: createFilePath({ node, getNode }),
+    })
+  }
+}
+
+// Ignore seamless-scroll-polyfill library when building due to references to document
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /seamless-scroll-polyfill/,
+            use: loaders.null(),
+          },
+        ],
+      },
     })
   }
 }
