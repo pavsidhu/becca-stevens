226c380095e21c490356d1341213259b5892df99
Pav Sidhu <pav@pavsidhu.com>
2020-05-03 23:11:25 +0100

    Create blog list page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import { Layout, SEO } from "../components"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Blog" />
+    </Layout>
+  )
+}
