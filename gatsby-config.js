045690ad2aa4bf379f6d26b90ba22457405ff865
Pav Sidhu <pav@pavsidhu.com>
2020-05-10 17:54:57 +0100

    Add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         theme_color: `#CA8080`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`,
+        cache_busting_mode: "none",
       },
     },
     {
@@ -74,8 +75,13 @@ module.exports = {
         ],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-offline",
+      options: {
+        workboxConfig: {
+          globPatterns: ["**/*"],
+        },
+      },
+    },
   ],
 }
