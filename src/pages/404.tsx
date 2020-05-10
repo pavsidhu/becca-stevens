15a19164b08fdd783f6c511b5309b47017bf8212
Pav Sidhu <pav@pavsidhu.com>
2020-05-10 17:09:30 +0100

    Add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,39 @@
 import React from "react"
+import styled from "styled-components"
 
-import { Layout, SEO } from "../components"
+import { Layout, SEO, Title, Button } from "../components"
+
+const Container = styled.article`
+  display: grid;
+  grid-template-areas:
+    ". .           ."
+    ". title       ."
+    ". .           ."
+    ". home-button ."
+    ". .           .";
+  grid-template-columns: 24px 1fr 24px;
+  grid-template-rows: 1fr auto 24px auto 2fr;
+  justify-items: center;
+  min-height: 100%;
+`
+
+const NotFoundTitle = styled(Title)`
+  grid-area: title;
+`
+
+const HomeButton = styled(Button)`
+  grid-area: home-button;
+`
 
 export default function NotFoundPage() {
   return (
     <Layout>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
+      <SEO title="Page Not Found" />
+
+      <Container>
+        <NotFoundTitle>Page Not Found</NotFoundTitle>
+        <HomeButton>Go Back Home</HomeButton>
+      </Container>
     </Layout>
   )
 }
