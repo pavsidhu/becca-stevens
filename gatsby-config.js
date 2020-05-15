b4389cfbd0b05a0b1c10356a5e8bdd682448fbbe
Pav Sidhu <pav@pavsidhu.com>
2020-05-15 22:50:03 +0100

    Add manifest description

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 module.exports = {
   siteMetadata: {
     title: `Maintain Fitness`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Hey I'm Becca, I’m a vegan Personal Trainer from Surrey, UK. I have a passion for functional fitness, healthy food and being outdoors.`,
     author: `@pavsidhu`,
   },
   plugins: [
