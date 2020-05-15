e9c6bf302e8888ee5d85baf30673d0e38fae8a70
Pav Sidhu <pav@pavsidhu.com>
2020-05-15 19:11:22 +0100

    Remove animations on reduced-motion

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,6 +33,20 @@ const GlobalStyle = createGlobalStyle`
   :root {
     --header-height: 96px;
   }
+
+  @media (prefers-reduced-motion: reduce) {
+    *,
+    ::before,
+    ::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-delay: 0s !important;
+      transition-duration: 0s !important; 
+    } 
+  }
 `
 
 const timeout = 400
